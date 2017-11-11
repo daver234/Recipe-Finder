b510bc92f5c306ac3e2d3723b7d853687d3f6186
daver234 <dave.rothschild@gmail.com>
2017-11-10 21:42:04 -0800

    - Adding Podfiles because I had to modify the source of CheckMark - Changed line 238 in M13Checkbox.swift to fileprivate var controller: M13CheckboxController = M13CheckboxStrokeController()

---
+++ Pods/Target Support Files/Pods-BlueCartTests/Pods-BlueCartTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlueCartTests : NSObject
+@end
+@implementation PodsDummy_Pods_BlueCartTests
+@end
---
+++ Pods/Target Support Files/Pods-BlueCartUITests/Pods-BlueCartUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlueCartUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BlueCartUITests
+@end
