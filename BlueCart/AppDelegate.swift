4eede71a574936b77ddcae2adb612868d3fb851b
daver234 <dave.rothschild@gmail.com>
2017-11-13 20:57:19 -0800

    - Add more UI tests - Enable reset state by passing command line arguments to AppDelegate when running UI tests

--- BlueCart/AppDelegate.swift
+++ BlueCart/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Used for UI testing to reset the application's state
+        if CommandLine.arguments.contains("--uitesting") {
+            resetState()
+        }
         return true
     }
 
@@ -126,5 +131,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 
             }
         }
+    }
+}
+
+private extension AppDelegate {
+    func resetState() {
+        let defaultsName = Bundle.main.bundleIdentifier!
+        UserDefaults.standard.removePersistentDomain(forName: defaultsName)
     }
 }
--- BlueCartUITests/BlueCartUITests.swift
+++ BlueCartUITests/BlueCartUITests.swift
@@ -9,8 +9,11 @@
 import XCTest
 
 let RECIPE_TVC_UITEST = "recipeTableVC"
+let RECIPE_CELL = "RecipeCell"
 
 class BlueCartUITests: XCTestCase {
+    
+    var app: XCUIApplication!
     
     override func setUp() {
         super.setUp()
@@ -20,35 +23,55 @@ class BlueCartUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        // XCUIApplication().launch()
+        app = XCUIApplication()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        /// Sending a command line agrument to the app to enable it to reset its state
+        app.launchArguments.append("--uitesting")
     }
     
+    /// So far, one of the top rated recipes has been this one. So check for it.
     func testShowTopRated() {
-        XCUIApplication().launch()
-        let app = XCUIApplication()
+        app.launch()
         let label = app.staticTexts["Perfect Iced Coffee"]
         self.waitForElementToAppear(element: label)
         XCTAssert(app.staticTexts["Perfect Iced Coffee"].exists)
     }
     
+    
+    /// Checks to see if accessability indicator is present in ReceipeTableVC
+    /// Indicator is set in ViewDidLoad
+    /// If there, then the app launched.
     func testRecipeTableVCisDisplaying() {
         XCUIApplication().launch()
-        let app = XCUIApplication()
-        XCTAssert(app.isDisplayingRecipeTableVC)
+        let appNew = XCUIApplication()
+        XCTAssert(appNew.isDisplayingRecipeTableVC)
     }
     
+    /// Launch app, tap search bar, enter text "apple", tap cancel
+    func testSearchBar() {
+        XCUIApplication().launch()
+        let appNew = XCUIApplication()
+        appNew/*@START_MENU_TOKEN@*/.tables/*[[".otherElements[\"recipeTableVC\"].tables",".tables"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.searchFields["Search for recipes..."].tap()
+        appNew.searchFields["Search for recipes..."].typeText("apple")
+        appNew.buttons["Cancel"].tap()
+        
+    }
 
+    /// Tap on a cell, go to RecipeDetail, tap a check mark, then tap Back
+    /// to go back to main RecipeTableVC
+    func testTapCell() {
+        XCUIApplication().launch()
+        let appNew = XCUIApplication()
+        appNew/*@START_MENU_TOKEN@*/.tables.staticTexts["Jalapeno Popper Grilled Cheese Sandwich"]/*[[".otherElements[\"recipeTableVC\"].tables",".cells.staticTexts[\"Jalapeno Popper Grilled Cheese Sandwich\"]",".staticTexts[\"Jalapeno Popper Grilled Cheese Sandwich\"]",".tables"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
+        appNew.tables.cells.containing(.staticText, identifier:"2 jalapeno peppers, cut in half lengthwise and seeded").children(matching: .other).element(boundBy: 0).tap()
+        appNew.navigationBars["Recipe"].buttons["Back"].tap()
+        
+    }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func waitForElementToAppear(element: XCUIElement, timeout: TimeInterval = 5,  file: String = #file, line: UInt = #line) {
@@ -64,7 +87,6 @@ class BlueCartUITests: XCTestCase {
             }
         }
     }
-    
 }
 
 /// For UI testing
