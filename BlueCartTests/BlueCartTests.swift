f0ef1e980ae5ee7241113588077f0b34c36aff08
daver234 <dave.rothschild@gmail.com>
2017-11-13 17:41:33 -0800

    - Begin to add unit tests

--- BlueCartTests/BlueCartTests.swift
+++ BlueCartTests/BlueCartTests.swift
@@ -7,13 +7,45 @@
 //
 
 import XCTest
-@testable import BlueCart
+@testable import Recipe_Finder
 
 class BlueCartTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func testCheckMockJsonData() {
+        let expectationResult = expectation(description: "Parse the test JSON Data to see if the second recipe has a name of Baked Doughnuts.")
+        do {
+            guard let file = Bundle.main.url(forResource: "recipes", withExtension: "json") else { return }
+            let data = try Data(contentsOf: file)
+            DataManager.instance.decodeDataForPage(data: data) { (response) in
+                guard response else {
+                    print("Error in APIManager response in testCheckMockJsonData")
+                    return
+                }
+                print("data: ",  DataManager.instance.allRecipes[0].recipes![1].title ?? 0)
+                guard let title = DataManager.instance.allRecipes[0].recipes![1].title else { return }
+                print("Here is title: ", title)
+                if title == "Baked Doughnuts" {
+                    XCTAssertTrue(response)
+                    expectationResult.fulfill()
+                } else {
+                    print("Error parsing local JSON data for product rating")
+                }
+                
+            }
+        } catch let jsonError {
+            print("error in test case parsing mock json \(jsonError.localizedDescription)")
+        }
+        
+        waitForExpectations(timeout: 10) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
     }
     
     override func tearDown() {
